0b5480868121258c53e833925749511a0ac40537
Elle Wilson <eleanorwilsonn@gmail.com>
2018-01-24 17:08:56 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,14 @@
 
-
+board = ["#{board[0]}", "#{board[1]}", "#{board[2]}", 
+      "#{board[3]}", "#{board[4]}", "#{board[5]}", "#{board[6]}", 
+      "#{board[7]}", "#{board[8]}"]
+      
+counter = 0
 def turn_count(board)
-
+  if board.each do |count|
+    puts count
 end
 
 def current_player(board)
-
+  if counter == odd
 end
