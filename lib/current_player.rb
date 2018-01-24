d60fdae7d55cf0f19448e2913977cf91205f6ed5
Elle Wilson <eleanorwilsonn@gmail.com>
2018-01-24 17:12:55 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 
-board = ["#{board[0]}", "#{board[1]}", "#{board[2]}",
+board = "#{board[0]}", "#{board[1]}", "#{board[2]}",
       "#{board[3]}", "#{board[4]}", "#{board[5]}", "#{board[6]}",
-      "#{board[7]}", "#{board[8]}"]
+      "#{board[7]}", "#{board[8]}"
 
 counter = 0
 
