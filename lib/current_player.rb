5d9f9326be8e0bf38f2eb7b296b315d91ef8b997
Elle Wilson <eleanorwilsonn@gmail.com>
2018-01-24 17:16:05 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,4 @@
 
-board = "#{board[0]}", "#{board[1]}", "#{board[2]}",
-      "#{board[3]}", "#{board[4]}", "#{board[5]}", "#{board[6]}",
-      "#{board[7]}", "#{board[8]}"
-
 counter = 0
 
 def turn_count(board)
